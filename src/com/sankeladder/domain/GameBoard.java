a7bf1dce369b62088abefa9c274a94b58bb187d8
Milan Pandey <milanpandey108@gmail.com>
2015-07-20 20:49:36 +0530

    Changed Snakes and ladder positions to fill automatically

--- src/com/sankeladder/domain/GameBoard.java
+++ src/com/sankeladder/domain/GameBoard.java
@@ -2,6 +2,7 @@ package com.sankeladder.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import com.sankeladder.enums.SurpriseType;
 
@@ -11,34 +12,86 @@ public class GameBoard {
 	public static final int BOARD_WIDTH = 12;
 	public static final int START = 0;
 	public static final int END = BOARD_LENGTH * BOARD_WIDTH;
+	public static final int NO_OF_SNAKES = 7;
+	public static final int NO_OF_LADDERS = 7;
 	
 	private Map<Integer, Surprise> positions = null;
+	private Random random = new Random();
 	
 	private void populateGameBoard(){
 		positions = new HashMap<Integer, Surprise>();
 		
-		positions.put(119, new Surprise(SurpriseType.SNAKE, 78));
-		positions.put(115, new Surprise(SurpriseType.SNAKE, 65));
-		positions.put(113, new Surprise(SurpriseType.SNAKE, 73));
-		positions.put(87, new Surprise(SurpriseType.SNAKE, 24));
-		positions.put(64, new Surprise(SurpriseType.SNAKE, 60));
-		positions.put(62, new Surprise(SurpriseType.SNAKE, 19));
-		positions.put(54, new Surprise(SurpriseType.SNAKE, 34));
+		fillSnakesPositions();
+		fillLadderPositions();
 		
-		positions.put(4, new Surprise(SurpriseType.LADDER, 14));
-		positions.put(9, new Surprise(SurpriseType.LADDER, 31));
-		positions.put(20, new Surprise(SurpriseType.LADDER, 38));
-		positions.put(28, new Surprise(SurpriseType.LADDER, 94));
-		positions.put(51, new Surprise(SurpriseType.LADDER, 67));
-		positions.put(63, new Surprise(SurpriseType.LADDER, 91));
-		positions.put(71, new Surprise(SurpriseType.LADDER, 111));
+	}
+	
+	private void fillSnakesPositions(){
+		int boardSize = BOARD_LENGTH * BOARD_WIDTH;
+		int head = 0;
+		int tail = 0;
+		
+		for(int i = 0; i < NO_OF_SNAKES; i++){
+			head = generateRandomPosition(boardSize/2, boardSize);
+			tail = generateRandomPosition(1, boardSize);
+			if(head > tail && positions.get(head) == null){
+				positions.put(head, new Surprise(SurpriseType.SNAKE, tail));
+			}else{
+				i--;
+			}
+		}
+	}
+	private void fillLadderPositions(){
+		int boardSize = BOARD_LENGTH * BOARD_WIDTH;
+		int firstStep = 0;
+		int lastStep = 0;
+		for(int i = 0; i < NO_OF_LADDERS; i++){
+			firstStep = generateRandomPosition(1, boardSize);
+			lastStep = generateRandomPosition(boardSize/2, boardSize);
+			if(lastStep > firstStep && positions.get(firstStep) == null){
+				positions.put(firstStep, new Surprise(SurpriseType.LADDER, lastStep));
+			}else{
+				i--;
+			}
+		}
+	}
+	
+	
+	
+	private int generateRandomPosition(int min, int max){
+		return random.nextInt((max - min) + 1) + min;
 	}
 
 	public Map<Integer, Surprise> getPositions() {
 		if(positions == null){
 			populateGameBoard();
+			printSnakeLadderPositions();
 		}
 		return positions;
+	}
+	
+	public void printSnakeLadderPositions(){
+		Map<Integer, Integer> snakes = new HashMap<>();
+		Map<Integer, Integer> ladder = new HashMap<>();
+		for(Map.Entry<Integer, Surprise> entry : positions.entrySet()){
+			Integer key = entry.getKey();
+			Surprise surprise = entry.getValue();
+			if(surprise.getSurpriseType().equals(SurpriseType.LADDER)){
+				ladder.put(key, surprise.getDestination());
+			}else{
+				snakes.put(key, surprise.getDestination());
+			}
+		}
+		System.out.print("Snakes positions: ");
+		for(Map.Entry<Integer, Integer> entry : snakes.entrySet()){
+			System.out.print("[" + entry.getKey() + " --> " + entry.getValue() + "] ");
+		}
+		System.out.println();
+		System.out.print("Ladder positions: ");
+		for(Map.Entry<Integer, Integer> entry : ladder.entrySet()){
+			System.out.print("[" + entry.getKey() + " --> " + entry.getValue() + "] ");
+		}
+		System.out.println();
 	}
 	
 }
